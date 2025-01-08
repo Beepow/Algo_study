0ae35a4e0d74ad66a17af1dc4a0ed6289e08e88a
Beepow <114973057+Beepow@users.noreply.github.com>
2025-01-08 15:39:08 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 328 ms, Memory: 32472 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,20 @@
+import sys
+
+T = int(sys.stdin.readline().rstrip())
+
+def recursion(s, l, r, cnt):
+    if l >= r:
+        return 1, cnt
+    elif s[l] != s[r] :
+        return 0, cnt
+    else :
+        return recursion(s, l+1, r-1, cnt+1)
+
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1, 1)
+
+for _ in range(T):
+    S = sys.stdin.readline().rstrip()
+    res, cnt = isPalindrome(S)
+    print(res, cnt)
