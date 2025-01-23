f79e84b22250bebd4698c06644ebabcd8565fdca
Beepow <114973057+Beepow@users.noreply.github.com>
2025-01-23 17:48:28 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 612 ms, Memory: 77724 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,28 @@
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+N, M, R = map(int, input().rstrip().split())
+graph = {i: [] for i in range(1, N + 1)}
+
+for _ in range(M):
+    u, v = map(int, input().rstrip().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for key in graph:
+    graph[key].sort(reverse=True)
+
+visited = [0]*(N+1)
+cnt = 1
+def dfs(R):
+    global cnt
+    visited[R] = cnt
+    for i in graph[R]:
+        if visited[i]==0:
+            cnt += 1
+            dfs(i)
+
+dfs(R)
+print("\n".join(map(str, visited[1:])))
